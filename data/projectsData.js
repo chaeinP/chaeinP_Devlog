25fb894fa9bbfc237b232cf0db7bd54f1a4ba17e
chaeinP <pca0046@gmail.com>
2022-01-13 16:01:52 +0900

    [update]poject page

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'Munetic',
     description: `Munetic 기업 협력 프로젝트`,
     imgSrc: '/static/images/ocean.jpeg',
-    // href: 'https://www.google.com',
+    href: 'https://chaeinp.vercel.app/tags/munetic',
   },
 ]
 
