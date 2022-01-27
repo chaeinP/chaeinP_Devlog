90c13f54902ace044221950fbacad60f47be8754
chaeinP <pca0046@gmail.com>
2022-01-27 22:05:01 +0900

    [create]new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,12 @@ const projectsData = [
     imgSrc: '/static/images/ocean.jpeg',
     href: 'https://chaeinp.vercel.app/tags/munetic',
   },
+  {
+    title: '42Checkin',
+    description: `42서울 Checkin Backend`,
+    imgSrc: '/static/images/42checkin.png',
+    href: 'https://chaeinp.vercel.app/tags/42checkin',
+  },
 ]
 
 export default projectsData
